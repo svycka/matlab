9f5099e8f0e7d8a4ca44ad9a4630d09fb5601e8d
gediminas <zarokas@gmail.com>
2014-11-21 00:10:56 +0200

    triangle collisions...not working

--- CollisionDetection.m
+++ CollisionDetection.m
@@ -98,6 +98,52 @@ classdef CollisionDetection
             end
          end
          
+         function detectTriangleCollision(this, cor1, a1, b1, cor2, a2, b2, U2)
+              %besisukancio
+             pa1 = [cor1(1)-a1/2    cor1(2)+b1/2];
+             pb1 = [pa1(1)+a1        pa1(2)];
+             pc1 = [pa1(1)           pa1(2)-b1];
+             pd1 = [pa1(1)+a1         pa1(2)-b1];
+
+            xc=U2(1)+cor2(1);yc=U2(2)+cor2(2);phi=U2(3)+cor2(3);
+            coord=[-a2/2  a2/2  0;
+                   -b2/2 -b2/2  b2/2;
+                     1    1    1  ];   % staciakampis etalonineje padetyje
+
+            T=[cos(phi) -sin(phi) xc;
+               sin(phi)  cos(phi) yc;
+                  0         0      1 ];      % transformavimo matrica
+            coord=T*coord;
+            pa2 = [coord(1,1) coord(2,1)];
+            pb2 = [coord(1,2) coord(2,2)];
+            pc2 = [coord(1,3) coord(2,3)];
+            
+            colides = zeros(16,1);
+            colides(1) = this.detectLineCollision(pa1, pb1, pa2, pb2);
+            colides(2) = this.detectLineCollision(pa1, pb1, pa2, pc2);
+            colides(3) = this.detectLineCollision(pa1, pb1, pb2, pc2);
+            
+            colides(4) = this.detectLineCollision(pa1, pc1, pa2, pb2);
+            colides(5) = this.detectLineCollision(pa1, pc1, pa2, pc2);
+            colides(6) = this.detectLineCollision(pa1, pc1, pb2, pc2);
+            
+            colides(7) = this.detectLineCollision(pc1, pd1, pa2, pb2);
+            colides(8) = this.detectLineCollision(pc1, pd1, pa2, pc2);
+            colides(9) = this.detectLineCollision(pc1, pd1, pb2, pc2);
+            
+            colides(10) = this.detectLineCollision(pb1, pd1, pa2, pb2);
+            colides(11) = this.detectLineCollision(pb1, pd1, pa2, pc2);
+            colides(12) = this.detectLineCollision(pb1, pd1, pb2, pc2);
+            
+            result = 1;
+            for j=1:12,
+               result = result * colides(j);
+            end
+            if result == 0
+                'trikampis kertasi'
+            end
+         end
+         
          function colides = detectLineCollision(~, corA1, corA2, corB1, corB2)
              A1 = corA2(2)-corA1(2);
              A2 = corB2(2)-corB1(2);
@@ -112,6 +158,7 @@ classdef CollisionDetection
              else
                 x = (B2*C1 - B1*C2)/det;
                 y = (A1*C2 - A2*C1)/det;
+                [x y]
                 if (x > corB1(1) && x < corB2(1)) && (y < corB1(2) && y > corB2(2))
                     colides = 0;
                 else
